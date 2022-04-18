447289b6df176634b3fcc6b05c49a6233eb087c8
SamuraiT <t.yasukawa01@gmail.com>
2022-04-18 15:23:30 +0900

    feat: Add chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import { Provider } from 'wagmi'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Provider autoConnect>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </Provider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import type { NextPage } from 'next'
 import { useAccount } from 'wagmi'
+import { Button, Text, Container, Heading } from '@chakra-ui/react'
+
 
 const Home: NextPage = () => {
   const [{ data, error, loading }, disconnect] = useAccount()
 
   return (
     <>
-      <div>{data?.address}</div>
-      <button onClick={disconnect}>
-        Disconnect from {data?.connector?.name}
-      </button>
+      <Container >
+        <Heading mt={50}>welcome to henkaku</Heading>
+        <Text>{data?.address}</Text>
+        <Button mt={10} colorScheme='teal' variant='solid' onClick={disconnect}>
+          Disconnect from {data?.connector?.name}
+        </Button>
+      </Container>
     </>
   )
 }
