17a68cb4760ac27dcedd1e1073152656a8aec18f
Alejandro Cremades <ale@alecrem.com>
2022-04-24 17:42:38 +0900

    Separate "Quests" into its own page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
+import NextLink from 'next/link'
 import { useAccount } from 'wagmi'
-import { Button, Text, Container, Heading, Image, Box } from '@chakra-ui/react'
+import { Button, Text, Container, Heading, Image, Box, Link, Flex, Spacer } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   const [{ data, error, loading }, disconnect] = useAccount()
@@ -8,26 +9,30 @@ const Home: NextPage = () => {
   return (
     <>
       <Box padding='4'>
-        <Heading size='md'>Omise – HENKAKU</Heading>
-        <Container>
-          <Heading mt={50}>Quest</Heading>
-          <Box display={{ md: 'flex', xl: 'flex' }}>
-            <Box p={2} minW={300}>
-              <Image src='/joi-ito-henkaku-podcast.png' alt='JOI ITO 変革への道' />
-            </Box>
-            <Box p={2}>
-              <Box bg='#ccc' w='100%' p={4}>
-                <Heading size='md'>Enter weekly keyword</Heading>
-                <Text>
-                  By answering this question you will earn 100 points, which you can exchange to $HENKAKU tokens.
-                </Text>
-              </Box>
-              <Text fontSize='xs'>{data?.address}</Text>
-              <Button mt={10} backgroundColor='#3c859d' color='#fff' variant='solid' onClick={disconnect}>
-                Disconnect from {data?.connector?.name}
-              </Button>
-            </Box>
+        <Flex>
+          <Box p={2}>
+            <Heading size='md'>Omise – HENKAKU</Heading>
           </Box>
+          <Spacer />
+          <Box p={2}>
+          <NextLink href='/' passHref>
+            <Link p={4} color='#3c859d' _hover={{}} borderBottom='1px'>
+                Home
+              </Link>
+            </NextLink>
+            <NextLink href='/quests' passHref>
+            <Link p={4} _hover={{}}>{/* Override the default text-decoration: underline on hover */}
+                Quests
+              </Link>
+            </NextLink>
+          </Box>
+        </Flex>
+        <Container>
+          <Heading mt={50}>Front page</Heading>
+          <Text fontSize='xs'>{data?.address}</Text>
+          <Button mt={10} backgroundColor='#3c859d' color='#fff' variant='solid' onClick={disconnect}>
+            Disconnect from {data?.connector?.name}
+          </Button>
         </Container>
       </Box>
     </>
