545062d09c487db47e5ccf8804d0eeff6d4b173e
hourglasshoro <hourglasshoro.628@gmail.com>
2022-05-03 18:55:26 +0900

    FIx Quest top page

--- pages/quests/index.tsx
+++ pages/quests/index.tsx
@@ -1,50 +1,48 @@
 import type { NextPage } from 'next'
-import NextLink from 'next/link'
-import { useAccount } from 'wagmi'
-import { Text, Container, Heading, Image, Box, Link, Flex, Spacer } from '@chakra-ui/react'
+import { Text, Heading, Image, Box, Button } from '@chakra-ui/react'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { Layout } from '@/components/layouts/layout'
+import { useMounted } from '@/hooks/useMounted'
 
 const Quests: NextPage = () => {
-  const { data, isError, isLoading } = useAccount()
+  const mounted = useMounted()
+  const { connect, connectors } = useConnect()
+  const [metaMask] = connectors
+  const { data } = useAccount()
 
   return (
     <>
-      <Box padding='4'>
-      <Flex>
+      <Layout>
+        <Heading mt={50}>Quest</Heading>
+        <Box display={{ md: 'flex', xl: 'flex' }}>
+          <Box p={2} minW={300}>
+            <Image
+              src="/joi-ito-henkaku-podcast.png"
+              alt="JOI ITO 変革への道"
+            />
+          </Box>
           <Box p={2}>
-            <Heading size='md'>Omise – HENKAKU</Heading>
-          </Box>
-          <Spacer />
-          <Box p={2}>
-            <NextLink href='/' passHref>
-              <Link p={4} _hover={{}}>{/* Override the default text-decoration: underline on hover */}
-                Home
-              </Link>
-            </NextLink>
-            <NextLink href='/quests' passHref>
-              <Link p={4} color='#3c859d' _hover={{}} borderBottom='1px'>
-                Quests
-              </Link>
-            </NextLink>
-          </Box>
-        </Flex>
-        <Container>
-          <Heading mt={50}>Quest</Heading>
-          <Box display={{ md: 'flex', xl: 'flex' }}>
-            <Box p={2} minW={300}>
-              <Image src='/joi-ito-henkaku-podcast.png' alt='JOI ITO 変革への道' />
+            <Box bg="lightgray" w="100%" p={4}>
+              <Heading size="md">Enter weekly keyword</Heading>
+              <Text>
+                By answering this question you will earn 100 points, which you
+                can exchange to $HENKAKU tokens.
+              </Text>
             </Box>
-            <Box p={2}>
-              <Box bg='#ccc' w='100%' p={4}>
-                <Heading size='md'>Enter weekly keyword</Heading>
-                <Text>
-                  By answering this question you will earn 100 points, which you can exchange to $HENKAKU tokens.
-                </Text>
-              </Box>
-              <Text fontSize='xs'>{data?.address}</Text>
-            </Box>
+            {mounted && !data?.address && (
+              <Button
+                mt={10}
+                w="100%"
+                colorScheme="teal"
+                onClick={() => connect(metaMask)}
+              >
+                connect wallet
+              </Button>
+            )}
+            {/*TODO: Add form component */}
           </Box>
-        </Container>
-      </Box>
+        </Box>
+      </Layout>
     </>
   )
 }
