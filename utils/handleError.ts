97708ed09d33b46aab9fcc44b1f3287e765296f1
hourglasshoro <hourglasshoro.628@gmail.com>
2022-05-05 22:19:02 +0900

    Fix getting error message function

--- utils/handleError.ts
+++ utils/handleError.ts
@@ -1,9 +1,5 @@
-type ErrorWithData = Error & { data: { code: number; message: string } }
-
 export const getErrorMessageFromRaw = (error: Error): string => {
-  const e = error as ErrorWithData
-  const regexp =
-    /Error: VM Exception while processing transaction: reverted with reason string '([a-zA-Z :]+)'/
-  const result = regexp.exec(e.data.message)
+  const regexp = /execution reverted: ([a-zA-Z :]+)/
+  const result = regexp.exec(error.message)
   return result && result.length > 1 ? result[1] : ''
 }
