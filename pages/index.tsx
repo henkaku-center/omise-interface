b77d6d4a959b60a791309e198d894cd4eeda6eba
SamuraiT <t.yasukawa01@gmail.com>
2022-05-05 22:38:31 +0900

    feat: Add top page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import { useAccount } from 'wagmi'
-import { Button, Text, Heading } from '@chakra-ui/react'
+import { Button, Text, Heading, Image, SimpleGrid, Box, useDisclosure } from '@chakra-ui/react'
 import { Layout } from '@/components/layouts/layout'
 
 const Home: NextPage = () => {
@@ -9,8 +9,27 @@ const Home: NextPage = () => {
   return (
     <>
       <Layout>
-        <Heading mt={50}>Front page</Heading>
-        <Text fontSize='xs'>{data?.address}</Text>
+        <Heading as='h2' color='gray.600'>
+          Mint your Kamon - 家紋{' '}
+        </Heading>
+        <Text m='1rem'>kamon NFT is membership of henkaku community</Text>
+        <SimpleGrid columns={{ sm: 1, md: 1, lg: 2 }} spacing='10px'>
+          <div>
+            <Image src={'/kamonNFT.svg'} alt='' />
+          </div>
+          <div>
+            <Box w='100%' p={4} color='grey.600'>
+              <Heading as='h3' fontSize='1.2rem'>
+                Kamon - 家紋 NFT costs 1000 $HENKAKU
+              </Heading>
+              <Text m='1rem'>Excluding Gas fee</Text>
+              <Button as='a' href='/mintKamon' size='lg' colorScheme='teal'>
+                Go and Mint your NFT
+              </Button>
+              {mounted && <Text fontSize='xs' mt='10px' >{data?.address}</Text>}
+            </Box>
+          </div>
+        </SimpleGrid>
       </Layout>
     </>
   )
