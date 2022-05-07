883348230a149fc979d5b5bdf751911dfa0dfe82
SamuraiT <t.yasukawa01@gmail.com>
2022-05-07 15:24:30 +0900

    refactor: use useContractRead write, event instead of just contract

--- hooks/useApproval.ts
+++ hooks/useApproval.ts
@@ -1,6 +1,13 @@
-import { ethers, Signer } from "ethers"
-import { useEffect, useState } from "react"
-import { erc20ABI, useAccount, useConnect, useContract, useNetwork, useProvider, useSigner, useWebSocketProvider } from "wagmi"
+import { ethers } from "ethers"
+import { useState } from "react"
+import {
+  erc20ABI,
+  useAccount,
+  useContractRead,
+  useContractWrite,
+  useContractEvent,
+  useSigner
+} from "wagmi"
 
 const APPROVE_CALLBACK_STATUS = {
   PENDING: 1,
@@ -11,55 +18,61 @@ const APPROVE_CALLBACK_STATUS = {
 const useApprove = (erc20: string, spender: string) => {
   const [status, setStatus] = useState<number>()
   const { data: signer } = useSigner()
-  const contract = useContract({
-    addressOrName: erc20,
-    contractInterface: erc20ABI,
-    signerOrProvider: signer,
-  })
+  const { write: contractApprove } = useContractWrite(
+    {
+      addressOrName: erc20,
+      contractInterface: erc20ABI,
+      signerOrProvider: signer
+    },
+    'approve',
+    {
+      args: [spender, ethers.constants.MaxUint256],
+      onError(error) {
+        console.log('hi', error)
+        setStatus(APPROVE_CALLBACK_STATUS.FAIL)
+      },
+      onSuccess(data) {
+        setStatus(APPROVE_CALLBACK_STATUS.PENDING)
+        console.log(data)
+      }
+
+    })
 
   return {
     status: status,
-    approve: () => contract.approve(
-      spender,
-      ethers.constants.MaxUint256
-    )
-      .then(() => { setStatus(APPROVE_CALLBACK_STATUS.PENDING) })
-      .catch(() => { setStatus(APPROVE_CALLBACK_STATUS.FAIL) })
+    approve: () => contractApprove()
   }
 }
 
 const useApproval = (erc20: string, spenderAddress: string) => {
   const [approved, setApprove] = useState<boolean>()
-  const [isReady, setReady] = useState<boolean>(false)
   const { data } = useAccount()
-  const { data: signer } = useSigner(
-    {
-      onSuccess() {
-        setReady(true)
-      },
-    }
-  )
-  const contract = useContract({
+  useContractRead({
     addressOrName: erc20,
     contractInterface: erc20ABI,
-    signerOrProvider: signer,
-  })
+  },
+    'allowance',
+    {
+      args: [data?.address, spenderAddress],
+      onSuccess(data) {
+        setApprove(data?.gt(1000) ? true : false)
+      },
+    },
+  )
 
-  useEffect(() => {
-    if (signer && isReady) {
-      contract
-        .allowance(data?.address, spenderAddress)
-        .then((allowance: ethers.BigNumber) => {
-          setApprove(allowance.gt(1000) ? true : false)
-        })
-
-      const filter = contract.filters.Approval(data?.address, spenderAddress, null)
-      contract.on(filter, (address: string, spender: string, value: ethers.BigNumber) => {
-        setApprove(value.gt(1000) ? true : false)
-      })
+  useContractEvent(
+    {
+      addressOrName: erc20,
+      contractInterface: erc20ABI,
+    },
+    'Approval ',
+    (event) => {
+      const [owner, address, value] = event
+      if (owner == data?.address && address == spenderAddress) {
+        setApprove(value?.gt(1000) ? true : false)
+      }
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data?.address, signer, isReady])
+  )
 
   return approved
 }
