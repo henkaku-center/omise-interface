30f52dce8414829e7923cfa09e9afcd11579d2e3
SamuraiT <t.yasukawa01@gmail.com>
2022-05-07 14:02:47 +0900

    style: Make responsive

--- components/metaMaskAlert/SwitchNetworkAlert.tsx
+++ components/metaMaskAlert/SwitchNetworkAlert.tsx
@@ -1,32 +1,44 @@
 import { ArrowForwardIcon } from '@chakra-ui/icons'
-import { Alert, AlertIcon, Button } from '@chakra-ui/react'
+import {
+  Alert,
+  AlertIcon,
+  AlertDescription,
+  Button,
+  useMediaQuery
+} from '@chakra-ui/react'
 import React from 'react'
 import { chain, useNetwork } from 'wagmi'
 
 export const SwitchNetworkAlert: React.FC = () => {
   const { activeChain, switchNetwork } = useNetwork()
+  const [isDesktoppOrTablet] = useMediaQuery('(min-width:600px)')
 
   if (activeChain == chain.polygon) {
     return <></>
   }
 
-  return (
-    switchNetwork ? (
-      <Alert status="warning">
-        <AlertIcon />
+  return switchNetwork ? (
+    <Alert
+      status="warning"
+      flexDirection={isDesktoppOrTablet ? undefined : 'column'}
+    >
+      <AlertIcon />
+      <AlertDescription>
         You are using {activeChain?.name}. To use this app, switch to polygon
         mainnet
-        <br />
-        <Button
-          ml="1.5rem"
-          colorScheme="teal"
-          variant="outline"
-          rightIcon={<ArrowForwardIcon />}
-          onClick={() => switchNetwork(chain.polygon.id)}
-        >
-          switch to polygon
-        </Button>
-      </Alert>
-    ) : (<></>)
+      </AlertDescription>
+
+      <Button
+        ml="1rem"
+        colorScheme="teal"
+        variant="outline"
+        rightIcon={<ArrowForwardIcon />}
+        onClick={() => switchNetwork(chain.polygon.id)}
+      >
+        switch to polygon
+      </Button>
+    </Alert>
+  ) : (
+    <></>
   )
 }
