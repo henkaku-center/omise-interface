ae5e79734801daba9557ba6203cd3461e1760c71
SamuraiT <t.yasukawa01@gmail.com>
2022-05-10 22:51:41 +0900

    feat: Update abi

--- utils/contractAddress.ts
+++ utils/contractAddress.ts
@@ -16,7 +16,7 @@ const contractAddress: ContractAddress = {
     [chainId.rinkeby]: '0x6FDDbe89B90795Eb0652F80fc3dBC2c61e753b1C'
   },
   kamonNFT: {
-    [chainId.rinkeby]: '0x518F8837C3Cc1c6c58b1d908134396d8A8764F1a'
+    [chainId.rinkeby]: '0x73bf43b830fE47CdeEC85190e99b9a6FCe9b9aaB'
   }
 }
 
