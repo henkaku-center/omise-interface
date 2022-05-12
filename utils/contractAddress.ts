7c7c443c756bb8407285289e3f8a5cedd06d5147
SamuraiT <t.yasukawa01@gmail.com>
2022-05-13 00:03:27 +0900

    feat: Add goerli network

--- utils/contractAddress.ts
+++ utils/contractAddress.ts
@@ -13,16 +13,18 @@ interface getContractAddressArg {
 
 const contractAddress: ContractAddress = {
   henkakuErc20: {
-    [chainId.rinkeby]: '0x6FDDbe89B90795Eb0652F80fc3dBC2c61e753b1C'
+    [chainId.rinkeby]: '0x6FDDbe89B90795Eb0652F80fc3dBC2c61e753b1C',
+    [chainId.goerli]: '0x539BCf896f02459dBcB3a2F1D823d2E65DB7211C'
   },
   kamonNFT: {
-    [chainId.rinkeby]: '0x9D8b1775CbEE7ae3Cf9dAE3D2CaCBA4986d7df63'
+    [chainId.rinkeby]: '0x9D8b1775CbEE7ae3Cf9dAE3D2CaCBA4986d7df63',
+    [chainId.goerli]: '0x539BCf896f02459dBcB3a2F1D823d2E65DB7211C'
   }
 }
 
 const defaultChainID = process.env.production
   ? chainId.polygon
-  : chainId.rinkeby
+  : chainId.goerli
 
 const getContractAddress = ({ name, chainId }: getContractAddressArg) => {
   return contractAddress[name][chainId || defaultChainID]
