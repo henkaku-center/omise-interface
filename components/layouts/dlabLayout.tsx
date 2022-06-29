4e883cc9ff713d067aaf92fff7d4ec13f5c12d17
SamuraiT <t.yasukawa01@gmail.com>
2022-06-29 15:58:56 +0900

    feat: Add mint page for dalab-ws

---
+++ components/layouts/dlabLayout.tsx
@@ -0,0 +1,64 @@
+import {
+  Container,
+  Heading,
+  Box,
+  Link,
+  Flex,
+  Spacer,
+  useColorMode,
+  Button
+} from '@chakra-ui/react'
+import { default as NextLink } from 'next/link'
+import { useRouter } from 'next/router'
+import useTranslation from 'next-translate/useTranslation'
+import { SwitchNetworkAlert } from '@/components/metaMask/SwitchNetworkAlert'
+import { MetaMaskLeadBanner } from '@/components/metaMask/MetaMaskLeadBanner'
+import { DALabFooter } from '@/components/footer'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import setLanguage from 'next-translate/setLanguage'
+import { useEffect } from 'react'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const DAlabLayout = ({ children }: LayoutProps) => {
+  const router = useRouter()
+  const { t, lang } = useTranslation('dalabs')
+  const { colorMode, toggleColorMode } = useColorMode()
+  useEffect(() => {
+    setLanguage('ja')
+  }, [])
+
+  return (
+    <>
+      <SwitchNetworkAlert />
+      <MetaMaskLeadBanner />
+      <Box p="4">
+        <Flex>
+          <Box p={2}>
+            <Heading size="md">Degital Archtecture Lab</Heading>
+          </Box>
+          <Spacer />
+          <Box p={2}>
+            <Button size="md" onClick={toggleColorMode} p={4} m={2}>
+              {colorMode == 'dark' ? <SunIcon /> : <MoonIcon />}
+            </Button>
+
+            <Button
+              onClick={async () =>
+                await setLanguage(lang == 'en' ? 'ja' : 'en')
+              }
+            >
+              {lang == 'en' ? '日本語' : 'English'}
+            </Button>
+          </Box>
+        </Flex>
+      </Box>
+      <Container maxW="4xl">{children}</Container>
+      <DALabFooter />
+    </>
+  )
+}
+
+export { DAlabLayout }
