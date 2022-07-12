252e2ad7f5e7a48bc5e482fb0a149279927e1a94
Alejandro Cremades <ale@alecrem.com>
2022-07-12 15:16:07 +0900

    Bonus: localize commented out Badges link

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -55,9 +55,9 @@ const Layout = ({ children }: LayoutProps) => {
             <NextLink passHref href="/quests" locale={router.locale}>
               <Link p={4}>{t('QUESTS_LINK')}</Link>
             </NextLink>
-            {/* <Link href="/badges" p={4}>
-              Badge
-            </Link> */}
+            {/* <NextLink passHref href="/badges" locale={router.locale}>
+              <Link p={4}>{t('BADGES_LINK')}</Link>
+            </NextLink> */}
             <Button
               onClick={async () =>
                 await setLanguage(lang == 'en' ? 'ja' : 'en')
