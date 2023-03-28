fe8a260c610c7b59aa5a5829b89c5688bfe40b01
Masumi Kawasaki <geeknees@gmail.com>
2023-03-28 23:56:15 +0900

    reverse badge order

--- components/badges/BadgeBox.tsx
+++ components/badges/BadgeBox.tsx
@@ -4,17 +4,26 @@ import useTranslation from 'next-translate/useTranslation'
 import { displayValue } from '@/utils/bigNumber'
 import { useFetchTokenURIJSON } from '@/hooks/badge/useFetchMetaData'
 import { useTotalSupply } from '@/hooks/badge/useTotalSupply'
-import { useBadges, BadgeElement } from '@/hooks/badge/useBadges'
+import { useBadges } from '@/hooks/badge/useBadges'
+
+type BadgeItem = {
+  tokenId: number
+  mintable: boolean
+  transferable: boolean
+  amount: ethers.BigNumber
+  maxSupply: ethers.BigNumber
+  tokenURI: string
+}
 
 export const BadgeBox: React.FC<{
-  badge: BadgeElement
-  tokenId: number
+  badge: BadgeItem
   contractAddress: string
   prefix?: string
 }> = (data) => {
-  const [, , amount, maxSupply, tokenURI] = data.badge
+  const { tokenId, mintable, transferable, amount, maxSupply, tokenURI } =
+    data.badge
   const { t } = useTranslation('common')
-  const { totalSupply } = useTotalSupply(data.contractAddress, data.tokenId)
+  const { totalSupply } = useTotalSupply(data.contractAddress, tokenId)
 
   const { tokenURIJSON } = useFetchTokenURIJSON(tokenURI)
 
@@ -27,38 +36,58 @@ export const BadgeBox: React.FC<{
   if (inventory <= 0) {
     return (
       <Box boxShadow="xs" p="6" rounded="md" bg="whiteAlpha.100">
-        <Image mt={1} src={tokenURIJSON.image} alt={t('IMAGE_PREVIEW_ALT')} />
+        <Image
+          boxSize="124px"
+          mt={1}
+          mb={2}
+          src={tokenURIJSON.image}
+          alt={t('IMAGE_PREVIEW_ALT')}
+        />
         <Badge variant="outline" colorScheme="green">
           {displayValue(amount)} $HENKAKU
         </Badge>
         <Badge variant="outline" colorScheme="green">
           Supply: {inventory.toString()} / {maxSupply.toString()}
         </Badge>
-        <Badge mt="5" size="sm" colorScheme="red">
+        <Button
+          mt="5"
+          size="sm"
+          colorScheme="red"
+          variant="outline"
+          isDisabled={true}
+        >
           SOLD OUT
-        </Badge>
+        </Button>
       </Box>
     )
   }
   return (
     <Link
-      href={
-        data.prefix
-          ? `/${data.prefix}/${data.tokenId}`
-          : `/badges/${data.tokenId}`
-      }
+      href={data.prefix ? `/${data.prefix}/${tokenId}` : `/badges/${tokenId}`}
     >
       <Box boxShadow="xs" p="6" rounded="md" bg="whiteAlpha.100">
-        <Image mt={1} src={tokenURIJSON.image} alt={t('IMAGE_PREVIEW_ALT')} />
+        <Image
+          boxSize="124px"
+          mt={1}
+          mb={2}
+          src={tokenURIJSON.image}
+          alt={t('IMAGE_PREVIEW_ALT')}
+        />
         <Badge variant="outline" colorScheme="green">
           {displayValue(amount)} $HENKAKU
         </Badge>
         <Badge variant="outline" colorScheme="green">
           Supply: {inventory.toString()} / {maxSupply.toString()}
         </Badge>
-        <Button mt="5" size="sm" colorScheme="teal">
-          Mint this badge
-        </Button>
+        {mintable ? (
+          <Button mt="5" size="sm" colorScheme="teal">
+            Mint this badge
+          </Button>
+        ) : (
+          <Button mt="5" size="sm" variant="outline">
+            Not available
+          </Button>
+        )}
       </Box>
     </Link>
   )
--- pages/badges/index.tsx
+++ pages/badges/index.tsx
@@ -1,15 +1,26 @@
 import type { NextPage } from 'next'
 
 import { Layout } from '@/components/layouts/layout'
-import { SimpleGrid, Heading } from '@chakra-ui/react'
+import { SimpleGrid, Heading, Divider } from '@chakra-ui/react'
 import { useNetwork, useConnect } from 'wagmi'
 import { getContractAddress } from '@/utils/contractAddress'
-import { useBadges, BadgeElement } from '@/hooks/badge/useBadges'
+import { useBadges } from '@/hooks/badge/useBadges'
 import { useEffect, useState } from 'react'
 import { BadgeBox } from '@/components/badges/BadgeBox'
 import { ConnectMetaMask } from '@/components/metaMask/Connect'
 import useTranslation from 'next-translate/useTranslation'
+import { ethers } from 'ethers'
 
+type BadgeItem = {
+  tokenId: number
+  mintable: boolean
+  transferable: boolean
+  amount: ethers.BigNumber
+  maxSupply: ethers.BigNumber
+  tokenURI: string
+}
+
+const PAST_EVENT_ID = [5, 6, 7, 8, 9, 10, 11, 12]
 const Badges: NextPage = () => {
   const { t } = useTranslation('badge')
   const { activeChain } = useNetwork()
@@ -18,10 +29,41 @@ const Badges: NextPage = () => {
     chainId: activeChain?.id
   })
   const { badges } = useBadges(henkakuBadge)
-  const [badgeList, setBadgeList] = useState<BadgeElement[]>([])
+  const [badgeList, setBadgeList] = useState<BadgeItem[]>([])
+  const [pastBadgeList, setPastBadgeList] = useState<BadgeItem[]>([])
 
   useEffect(() => {
-    setBadgeList(badges?.reverse() as BadgeElement[])
+    const badgeArray = badges?.map((badge, index) => {
+      if (!PAST_EVENT_ID.includes(index + 1)) {
+        return {
+          tokenId: index + 1,
+          mintable: badge[0],
+          transferable: badge[1],
+          amount: badge[2],
+          maxSupply: badge[3],
+          tokenURI: badge[4]
+        }
+      } else {
+        return {}
+      }
+    })
+    setBadgeList(badgeArray as BadgeItem[])
+
+    const pastBadgeArray = badges?.map((badge, index) => {
+      if (!PAST_EVENT_ID.includes(index + 1)) {
+        return {}
+      } else {
+        return {
+          tokenId: index + 1,
+          mintable: badge[0],
+          transferable: badge[1],
+          amount: badge[2],
+          maxSupply: badge[3],
+          tokenURI: badge[4]
+        }
+      }
+    })
+    setPastBadgeList(pastBadgeArray as BadgeItem[])
   }, [badges])
 
   const { isConnected } = useConnect()
@@ -51,13 +93,27 @@ const Badges: NextPage = () => {
         rounded="lg"
       >
         {badgeList &&
-          badgeList.map((data, index) => (
-            <div key={index}>
-              <BadgeBox
-                badge={data}
-                tokenId={index + 1}
-                contractAddress={henkakuBadge}
-              />
+          badgeList.map((data) => (
+            <div key={data.tokenId}>
+              <BadgeBox badge={data} contractAddress={henkakuBadge} />
+            </div>
+          ))}
+      </SimpleGrid>
+      <Divider />
+      <Heading mt={5} as="h3" size="md">
+        Past Badges
+      </Heading>
+      <SimpleGrid
+        columns={{ sm: 2, md: 4 }}
+        spacing="8"
+        p="10"
+        textAlign="center"
+        rounded="lg"
+      >
+        {pastBadgeList &&
+          pastBadgeList.map((data) => (
+            <div key={data.tokenId}>
+              <BadgeBox badge={data} contractAddress={henkakuBadge} />
             </div>
           ))}
       </SimpleGrid>
